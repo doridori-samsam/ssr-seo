85377fa0c6151437cf1608b3e2d1b00b28274901
Sam <wtawonlife@gmail.com>
2024-11-21 00:45:52 +0900

    test metadata opengraph property

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ export const metadata = {
         title: "Nextjs로 SEO 테스트",
         description: "Nextjs를 안쓰는건 미친짓이다.",
         siteName: "SSR SEO 테스트",
-        image: [
+        images: [
             {
                 url: "https://d3kxs6kpbh59hp.cloudfront.net/community/COMMUNITY/9242ab46828d4360a19648d54691f497/4c56d329980b4d69baa2180101043594_1599932445.jpg",
                 width: 900,
